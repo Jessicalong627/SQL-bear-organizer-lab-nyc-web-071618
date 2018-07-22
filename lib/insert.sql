d0395e5c24b564a9a977fb7a44be45a1ccad351c
Jessicalong627 <jessicalong627@gmail.com>
2018-07-22 15:48:32 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', "Brown", "Crazy", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 4, 'M', "Black", "Goofy", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 12, 'F', "Grey", "Relaxed", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', "Brown", "Happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 1, 'F', "Brown", "Passive", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'M', "Brown", "Introverted", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'F', "Red", "Cool", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, 'M', "Black", "Crazy", 0);
